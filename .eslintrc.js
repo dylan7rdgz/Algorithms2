d95ec4e1918a39d4188c408b1e4e5865528bd44f
Dylan Rodrigues <dylan7rdgz@gmail.com>
2023-07-15 23:10:03 +0530

    initialized a package linter with baisc highlights

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+/*
+    Set of Rules picked up from JavaScript - The Definitive Guide
+
+    1. No colon after after right parenthesis of a for loop, while loop, or if statement;
+    2. Use for/of with Object.keys() instead of for/in while looping through objects;
+    3. Avoid the with statement
+    4. Use Strict mode - for more details see pg. no. 123 of the book.
+    5. Avoid using declarations with var
+
+
+*/
