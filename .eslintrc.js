57b0803471bcc1799d93841f0960666777b3b2e0
Dylan Rodrigues <dylan7rdgz@gmail.com>
2023-11-19 20:43:36 +0530

    updated the .eslintrc.js file

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,11 @@
     3. Avoid the with statement
     4. Use Strict mode - for more details see pg. no. 123 of the book.
     5. Avoid using declarations with var
+    6. Use Typed arrays wherever possible
+    7. If there is a chance to use function expressions instead of function declarations
+    8. Use Arrow functions
+    9. Note that, when writing an arrow function, you must not put a new line between the function parameters and the arrow
+    10. Predict when params passed to a function are a lot and difficult for the caller to remember - Suggest Destructuring
 
 
 */
